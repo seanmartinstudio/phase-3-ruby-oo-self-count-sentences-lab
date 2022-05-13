0e2b199f4b7729ebe116cb5dc4299145859229e4
Sean Martin <sean@seanmartinstudio.com>
2022-05-13 13:48:46 -0700

    commit tree 96afccd063e7d577c284c3bcf453a1bd23a106d4

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    str = self.split(/[[:punct:]]{1,}/)
+    str.filter do |arr|
+      if arr.empty?
+        arr.delete
+      end
+    end
+    str.count
   end
 end
+
